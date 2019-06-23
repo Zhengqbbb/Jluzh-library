5d9934dffe85a1b35d5add13879b6dba3be844bd
Zhengqbbb <1074059947@qq.com>
2019-06-23 23:55:41 +0800

    updata 新闻管理

--- admin/src/views/ads/NewEdit.vue
+++ admin/src/views/ads/NewEdit.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="about">
-    <h1> {{id? '修改' : '新建'}}新闻分类</h1>
+    <h1> {{id? '修改' : '新建'}}新闻</h1>
     <el-form label-width="120px" @submit.native.prevent="saveform('model')" :model="model" :rules="rules" ref="model">
-      <el-form-item label="分类名称" prop="name">
+      <el-form-item label="新闻标题" prop="name">
         <el-col :span="4">
           <el-input v-model="model.name" input-width="240px" clearable></el-input>
         </el-col>
       </el-form-item>
+
+      <el-form-item label="新闻分类:" prop="category">
+        <el-select v-model="model.category" placeholder="请选择新闻分类" label="图书分类">
+          <el-option v-for="item in categoryOption" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="新闻内容:" prop="message">
+        <el-col :span="7">
+          <el-input type="textarea" v-model="model.message"></el-input>
+        </el-col>
+      </el-form-item>
+
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
       </el-form-item>
@@ -22,6 +35,7 @@
     data() {
       return {
         model: {},
+        categoryOption: {},
         rules: {
           name: [{
               required: true,
