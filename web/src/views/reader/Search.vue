bb033bf8a3b02d84833615037314db9b2144557e
Zhengqbbb <1074059947@qq.com>
2019-07-06 04:51:24 +0800

    updata -> 优化馆内搜索

--- web/src/views/reader/Search.vue
+++ web/src/views/reader/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!--  馆内搜索 -->
-    <div class="search-card w-common p-5 bd text-center">
+    <div class="search-card pt-5 px-4 bd text-center">
       <div class="search-input">
         <el-col :span="10">
           <el-input class="search" v-model="search" prefix-icon="el-icon-search" clearable placeholder="输入图书名关键词搜索" />
@@ -9,19 +9,19 @@
       </div>
       <div class="search-table">
         <el-table :data="items">
-          <el-table-column fixed="left" width="100">
-             <template slot-scope="scope">
-            <el-button type="primary" size="small" @click="$router.push(`/book/infor/${scope.row._id}`)"
-              icon="el-icon-search">查看</el-button>
-          </template>
-          </el-table-column>
           <el-table-column prop="_id" label="图书编号" width="240"></el-table-column>
           <el-table-column prop="name" label="书名"></el-table-column>
           <el-table-column prop="category.name" label="图书分类" width="200"></el-table-column>
-          <el-table-column prop="author" label="作者" width="300"></el-table-column>
+          <el-table-column prop="author" label="作者" width="200"></el-table-column>
           <el-table-column prop="img" label="封面" width="200">
             <template slot-scope="scope">
               <img :src="scope.row.img" style="height:2.8rem;">
+            </template>
+          </el-table-column>
+            <el-table-column fixed="right" width="100">
+            <template slot-scope="scope">
+              <el-button type="primary" size="small" @click="$router.push(`/book/infor/${scope.row._id}`)"
+                icon="el-icon-search">查看</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -97,7 +97,7 @@
       this.fetch()
     },
     watch: {
-      search: function (new_v, old_v) {
+      search: function (new_v) {
         if (new_v != '') {
           this.searchItem()
         } else {
@@ -112,6 +112,8 @@
   @import '../../assets/scss/style';
 
   .search-card {
+    margin: 13px auto;
+    width: 75%;
     min-height: 780px;
     border-radius: 20px;
     box-shadow: 0 0 10px map-get($colors, 'grey');
